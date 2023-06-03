7423ea77a09aa6768dc70d741ee6330f0e5ac894
Francisco Hernández <2661815@alu.murciaeduca.es>
2023-06-03 11:42:18 +0200

    Apartado 3

---
+++ HernandezLeon/src/main/java/com/mycompany/hernandezleon/HernandezLeon.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.hernandezleon;
+
+/**
+ *
+ * @author Paco
+ */
+public class HernandezLeon {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
